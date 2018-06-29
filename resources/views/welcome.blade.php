2a1c043a2cb8460b3e2a27279cd5950984ce61cb
Vernon Lacerda Bacellar <vernon.bacellar@cprm.gov.br>
2018-06-29 14:08:38 -0300

    Teste meia boa gerador de Mimimi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Vernon Teste 2
+                    Laravel - Vernon Teste 3
                 </div>
 
                 <div class="links">
