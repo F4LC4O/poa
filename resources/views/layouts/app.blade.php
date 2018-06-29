ccdd66bff11d626308cf2930250b9d51730685b8
Túlio Falcão Ribeiro <tulio.ribeiro@cprm.gov.br>
2018-06-29 14:53:38 -0300

    Controladores de videoconferência e análise petrográfica criados. Navbar adicionado.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('titulo') - Meia-boca-teste</title>        
+        <title>@yield('titulo')</title>        
         
         <!-- CSS
         ================================================== -->       
@@ -27,6 +27,28 @@
 
     </head>
     <body>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Intranet CPRM</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/videoconferencia">Videoconferência</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/analisepetro">Análise Petrográfica</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div> 
+
         @yield('content')
         
         @yield('scripts')
